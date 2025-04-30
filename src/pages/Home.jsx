f256621215554eb80186a850e3afc3f8b7e15858
Muhammed <faraanshaik19@gmail.com>
2025-04-30 23:34:27 +0530

    skeleton updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PokemonCard from "../components/PokemonCard";
-import Spinner from "../components/Spinner";
+// import Spinner from "../components/Spinner";
+import Skeleton from "../components/Skeleton";
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -92,7 +93,8 @@ const Home = () => {
       </div>
 
       {loading ? (
-        <Spinner />
+        // <Spinner />
+        <Skeleton />
       ) : (
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
